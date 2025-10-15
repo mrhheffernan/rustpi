78c1f62211b7779eb124fc22fdf1b50465634711
Matthew Heffernan <5447232+mrhheffernan@users.noreply.github.com>
2025-10-14 21:34:21 -0700

    MVP of integrating a circle (#1)
    * Move generating a coordinate to a function, add timing
    
    * First implementation of checking radius
    
    * Add loop, pi calculation
    
    * Add python benchmark, notes, and minor tweaks

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,5 @@
 # rustpi
 
 The mathematical version of Hello, World: Calculating pi by integrating the unit circle.
+
+The implementation is duplicated in Python for benchmarking, which shows that `cargo run --release` yields a 20x speedup over the matching Python implementation, while without the release optimizations the Rust implementation is actually slower.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 use rand::Rng;
+use std::time::Instant;
 
-fn main() {
-    let mut rng = rand::rng();
+const RADIUS: f64 = 1.0;
+const RADIUS_SQ: f64 = RADIUS * RADIUS;
 
+fn generate_coord<T: Rng>(mut rng: T) -> (f64, f64) {
+    // Generate an x, y coordinate pair
     let x: f64 = rng.random();
     let y: f64 = rng.random();
 
-    println!("x: {x}");
-    println!("y: {y}");
+    (x, y)
+}
+
+fn main() {
+    let n_samples: i32 = 100000;
+    let mut rng = rand::rng();
+
+    let time_start = Instant::now();
+    // Draw samples and check
+    let mut count = 0;
+    for _i in 0..=n_samples {
+        let (x, y) = generate_coord(&mut rng);
+        let sample_radius_sq = x * x + y * y;
+        let in_circle: bool = sample_radius_sq <= RADIUS_SQ;
+        if in_circle {
+            count += 1
+        }
+    }
+    let time_end = Instant::now();
+
+    let duration = time_end.duration_since(time_start);
+    let duration_ms = duration.as_micros();
+    
+    let pi: f64 = 4.0 * count as f64 / n_samples as f64;
+
+    println!("pi: {pi}");    
+    println!("duration: {duration_ms} microseconds");
 }
