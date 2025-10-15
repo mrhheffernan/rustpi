c3dd630f2b11bf38092e9b3aae19a31c33741de1
Matthew Heffernan <mrhheffernan@gmail.com>
2025-10-14 21:15:53 -0700

    Add loop, pi calculation

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,28 @@ fn generate_coord<T: Rng>(mut rng: T) -> (f64, f64) {
 }
 
 fn main() {
-    let rng = rand::rng();
+    let n_samples: i32 = 10000;
+    let mut rng = rand::rng();
 
     let time_start = Instant::now();
-    let (x, y) = generate_coord(rng);
-    let sample_radius_sq = x * x + y * y;
+    let mut count = 0;
+    for _i in 0..=n_samples {
+        let (x, y) = generate_coord(&mut rng);
+        let sample_radius_sq = x * x + y * y;
+        let in_circle: bool = sample_radius_sq <= RADIUS_SQ;
+        if in_circle {
+            count += 1
+        }
+    }
     let time_end = Instant::now();
 
     let duration = time_end.duration_since(time_start);
     let duration_ms = duration.as_micros();
+    
+    let pi: f64 = 4.0 * count as f64 / n_samples as f64;
 
-    let in_circle: bool = sample_radius_sq <= RADIUS_SQ;
-
-    println!("x: {x}");
-    println!("y: {y}");
-    println!("in circle: {in_circle}");
+    println!("count: {count}");
+    println!("pi: {pi}");
+    
     println!("duration: {duration_ms} microseconds");
 }
