33070656a2946650d187ee289ed22398dbd559eb
Matthew Heffernan <5447232+mrhheffernan@users.noreply.github.com>
2025-10-14 21:57:53 -0700

    Add argument parsing (#2)

--- python_benchmark/main.py
+++ python_benchmark/main.py
@@ -1,9 +1,18 @@
+import argparse
 import numpy as np
 import time
 
 RADIUS = 1.0
 RADIUS_SQ = RADIUS * RADIUS
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--n_samples", 
+                      type=int,
+                      help="Number of samples")
+    
+    return parser.parse_args()
 
 def generate_coord(rng) -> tuple[float, float]:
     """Generate an x, y coordinate pair"""
@@ -13,7 +22,9 @@ def generate_coord(rng) -> tuple[float, float]:
 
 
 def main():
-    n_samples = 100000
+    args = parse_args()
+    n_samples = args.n_samples
+
     rng = np.random.default_rng()
 
     time_start = time.time()
@@ -26,6 +37,7 @@ def main():
             count += 1
     time_end = time.time()
 
+    print(f"n_samples: {n_samples}")
     print(f"pi: {4 * count/n_samples}")
     print(f"duration: {(time_end - time_start) * 10 ** 6}  microseconds")
 
