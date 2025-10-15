9b290d2f8cc6fa9504050e1e0a43fc1c08b6e5b9
Matthew Heffernan <mrhheffernan@gmail.com>
2025-10-14 20:55:45 -0700

    Move generating a coordinate to a function, add timing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use rand::Rng;
+use std::time::Instant;
 
-fn main() {
-    let mut rng = rand::rng();
-
+fn generate_coord<T: Rng>(mut rng: T) -> (f64, f64) {
     let x: f64 = rng.random();
     let y: f64 = rng.random();
 
+    (x, y)
+}
+
+fn main() {
+    let rng = rand::rng();
+
+    let time_start = Instant::now();
+    let (x, y) = generate_coord(rng);
+    let time_end = Instant::now();
+
+    let duration = time_end.duration_since(time_start);
+    let duration_ms = duration.as_micros();
+
     println!("x: {x}");
     println!("y: {y}");
+    println!("duration: {duration_ms} microseconds")
 }
