f2b9727eb47e6e6e28c22797e47bdb127fa24b80
Matthew Heffernan <mrhheffernan@gmail.com>
2025-10-15 19:52:09 -0700

    Add library, define module

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
 use clap::Parser;
-use rand::Rng;
+use mc_circle::utils::estimate_pi;
 use std::time::Instant;
-
-const RADIUS: f64 = 1.0;
-const RADIUS_SQ: f64 = RADIUS * RADIUS;
-
-fn generate_coord<T: Rng>(mut rng: T) -> (f64, f64) {
-    // Generate an x, y coordinate pair
-    let x: f64 = rng.random();
-    let y: f64 = rng.random();
-
-    (x, y)
-}
-
-fn estimate_pi<T: Rng>(&n_samples: &i32, mut rng: T) -> f64 {
-    let mut count = 0;
-    for _i in 0..=n_samples {
-        let (x, y) = generate_coord(&mut rng);
-        let sample_radius_sq = x * x + y * y;
-        let in_circle: bool = sample_radius_sq <= RADIUS_SQ;
-        if in_circle {
-            count += 1
-        }
-    }
-
-    let pi: f64 = 4.0 * count as f64 / n_samples as f64;
-
-    pi
-}
 
 #[derive(Parser)]
 struct Args {
@@ -47,8 +20,8 @@ fn main() {
 
     let duration = time_end.duration_since(time_start);
     let duration_ms = duration.as_micros();
-    
+
     println!("n_samples: {n_samples}");
-    println!("pi: {pi}");    
+    println!("pi: {pi}");
     println!("duration: {duration_ms} microseconds");
 }
