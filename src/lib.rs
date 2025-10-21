c7af2e8c7268a2b7c022dae5c3481eb6719a7264
Matthew Heffernan <5447232+mrhheffernan@users.noreply.github.com>
2025-10-20 21:57:23 -0700

    Alternative parallelization (#6)
    * Build a vector of samples instead of two independent Vecs
    
    * Consolidate two iterations into 1
    
    * Typo
    
    * Use map_init to initialize an rng once per thread)

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,21 @@ const RADIUS_SQ: f64 = RADIUS * RADIUS;
 pub mod utils {
     use rand::Rng;
     use rayon::prelude::*;
-    fn generate_coords(n_samples: i32) -> (Vec<f64>, Vec<f64>) {
-        // the || is a way of getting an anonymous functino
-        let sampler = || {
-            let mut rng = rand::rng();
-            rng.random_range(0.0..1.0)
-        };
-
-        let x_vec = (0..n_samples).into_par_iter().map(|_| sampler()).collect();
-        let y_vec = (0..n_samples).into_par_iter().map(|_| sampler()).collect();
-
-        (x_vec, y_vec)
+    fn generate_coords(n_samples: i32) -> Vec<(f64, f64)> {
+        (0..n_samples)
+            .into_par_iter()
+            .map_init(rand::rng, |rng, _| {
+                (rng.random_range(0.0..1.0), rng.random_range(0.0..1.0))
+            })
+            .collect()
     }
 
     pub fn estimate_pi(&n_samples: &i32) -> f64 {
         let mut count = 0;
-        let (x, y) = generate_coords(n_samples);
+        let coords = generate_coords(n_samples);
 
         for i in 0..=n_samples - 1 {
-            let x_sample = x[i as usize];
-            let y_sample = y[i as usize];
+            let (x_sample, y_sample) = coords[i as usize];
 
             let sample_radius_sq = x_sample * x_sample + y_sample * y_sample;
             let in_circle: bool = sample_radius_sq <= super::RADIUS_SQ;
