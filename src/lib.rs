d8f7bad1e13312cbb0b2c1e81457403bf712f648
Matthew Heffernan <mrhheffernan@gmail.com>
2025-10-20 21:11:45 -0700

    Build a vector of samples instead of two independent Vecs

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,35 @@ const RADIUS_SQ: f64 = RADIUS * RADIUS;
 
 pub mod utils {
     use rand::Rng;
-    use rayon::prelude::*;
-    fn generate_coords(n_samples: i32) -> (Vec<f64>, Vec<f64>) {
+
+    fn generate_coords(n_samples: i32) -> Vec<(f64, f64)> {
         // the || is a way of getting an anonymous functino
         let sampler = || {
             let mut rng = rand::rng();
             rng.random_range(0.0..1.0)
         };
 
-        let x_vec = (0..n_samples).into_par_iter().map(|_| sampler()).collect();
-        let y_vec = (0..n_samples).into_par_iter().map(|_| sampler()).collect();
+        let mut pairs: Vec<(f64, f64)> = Vec::new();
+        rayon::ThreadPoolBuilder::new()
+            .build()
+            .unwrap()
+            .install(|| {
+                for _ in 0..n_samples {
+                    let x = sampler();
+                    let y = sampler();
+                    pairs.push((x, y))
+                }
+            });
 
-        (x_vec, y_vec)
+        pairs
     }
 
     pub fn estimate_pi(&n_samples: &i32) -> f64 {
         let mut count = 0;
-        let (x, y) = generate_coords(n_samples);
+        let coords = generate_coords(n_samples);
 
         for i in 0..=n_samples - 1 {
-            let x_sample = x[i as usize];
-            let y_sample = y[i as usize];
+            let (x_sample, y_sample) = coords[i as usize];
 
             let sample_radius_sq = x_sample * x_sample + y_sample * y_sample;
             let in_circle: bool = sample_radius_sq <= super::RADIUS_SQ;
