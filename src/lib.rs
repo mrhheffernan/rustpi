d1a5d38db7346e8be2abf1ed07e6f2fb6fc05d41
Matthew Heffernan <5447232+mrhheffernan@users.noreply.github.com>
2025-10-21 21:17:01 -0700

    Add polars, multiple samples (#8)
    * Add polars, multiple samples
    
    * Move central estimate to its own function, add n_estimates to args
    
    * Fix whitespace
    
    * Move function to lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ const RADIUS: f64 = 1.0;
 const RADIUS_SQ: f64 = RADIUS * RADIUS;
 
 pub mod utils {
+    use polars::prelude::*;
     use rand::Rng;
     use rayon::prelude::*;
     fn generate_coords(n_samples: i32) -> Vec<(f64, f64)> {
@@ -13,7 +14,7 @@ pub mod utils {
             .collect()
     }
 
-    pub fn estimate_pi(&n_samples: &i32) -> f64 {
+    fn estimate_pi(&n_samples: &i32) -> f64 {
         let coords = generate_coords(n_samples);
 
         let inside: Vec<bool> = coords
@@ -24,5 +25,19 @@ pub mod utils {
 
         let pi: f64 = 4.0 * count_map / n_samples as f64;
         pi
+    }
+
+    pub fn calculate_central_estimate(n_iterations: i32, &n_samples: &i32) -> (f64, f64) {
+        let mut estimates = std::vec::Vec::new();
+        for _i in 0..n_iterations {
+            let pi = estimate_pi(&n_samples);
+            estimates.push(pi);
+        }
+        let s = Series::new("Estimates".into(), &estimates);
+        let mean_pi: f64 = s.mean().unwrap();
+        let estimate_length = n_iterations as f64;
+        let std_err = s.std(1).unwrap() / estimate_length.sqrt();
+
+        (mean_pi, std_err)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use mc_circle::utils::estimate_pi;
+use mc_circle::utils::calculate_central_estimate;
 use std::time::Instant;
 
 #[derive(Parser)]
@@ -7,20 +7,23 @@ struct Args {
     // The pattern to look for
     #[arg(short, long)]
     n_samples: i32,
+    #[arg(short, long)]
+    n_estimates: i32,
 }
 
 fn main() {
     let args = Args::parse();
     let n_samples = args.n_samples;
+    let n_estimates = args.n_estimates;
 
     let time_start = Instant::now();
-    let pi = estimate_pi(&n_samples);
+    let (mean_pi, std_err) = calculate_central_estimate(n_estimates, &n_samples);
     let time_end = Instant::now();
 
     let duration = time_end.duration_since(time_start);
     let duration_ms = duration.as_micros();
 
     println!("n_samples: {n_samples}");
-    println!("pi: {pi}");
+    println!("mean estimate: {mean_pi} +/- {std_err}");
     println!("duration: {duration_ms} microseconds");
 }
