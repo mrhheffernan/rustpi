4b664d676484fcf78052b4e54825b917dd0998a5
Matthew Heffernan <mrhheffernan@gmail.com>
2025-10-21 21:09:43 -0700

    Move central estimate to its own function, add n_estimates to args

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,32 @@ struct Args {
     // The pattern to look for
     #[arg(short, long)]
     n_samples: i32,
+    #[arg(short, long)]
+    n_estimates: i32,
+}
+
+fn calculate_central_estimate(n_iterations: i32, &n_samples: &i32) -> (f64, f64) {
+    let mut estimates = std::vec::Vec::new();
+    for _i in 0..n_iterations {
+        let pi = estimate_pi(&n_samples);
+        estimates.push(pi);
+    }
+    let s = Series::new("Estimates".into(), &estimates);
+    let mean_pi: f64 = s.mean().unwrap();
+    let estimate_length = n_iterations as f64;
+    let std_err = s.std(1).unwrap() / estimate_length.sqrt();
+
+    (mean_pi, std_err)
 }
 
 fn main() {
     let args = Args::parse();
     let n_samples = args.n_samples;
-    const N_ESTIMATES: i32 = 10;
+    let n_estimates = args.n_estimates;
 
     let time_start = Instant::now();
 
-    let mut estimates = std::vec::Vec::new();
-    for _i in 0..N_ESTIMATES {
-        let pi = estimate_pi(&n_samples);
-        estimates.push(pi);
-    }
-
-    let s = Series::new("Estimates".into(), &estimates);
-    let mean_pi: f64 = s.mean().unwrap();
-    let estimate_length = N_ESTIMATES as f64;
-    let std_err = s.std(1).unwrap() / estimate_length.sqrt();
+    let (mean_pi, std_err) = calculate_central_estimate(n_estimates, &n_samples);
 
     let time_end = Instant::now();
 
